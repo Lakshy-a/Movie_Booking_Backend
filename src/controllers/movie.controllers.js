1c85568d0654dc85d9e8541ea5b0df1c08dfc529
Lakshya Gupta <lakshya.gupta@adglobal360.in>
2025-10-29 16:50:24 +0530

    get movie by id api

--- src/controllers/movie.controllers.js
+++ src/controllers/movie.controllers.js
@@ -1,6 +1,7 @@
 import { Movie } from "../models/movie.model.js";
 import { successResponse, errorResponse } from "../utils/responsehandler.utils.js";
 
+// to create a new movie
 export const createMovie = async (req, res) => {
     try {
         const movie = await Movie.create(req.body);
@@ -11,6 +12,7 @@ export const createMovie = async (req, res) => {
     }
 }
 
+// to delete a movie by id
 export const deleteMovie = async (req, res) => {
     try {
         const { movieId } = req.params;
@@ -19,5 +21,22 @@ export const deleteMovie = async (req, res) => {
     } catch (error) {
         console.error('Error deleting movie:', error);
         return errorResponse(res, 500, 'Internal Server Error', 'An error occurred while deleting the movie');
+    }
+}
+
+// to fetch the movie by id
+export const getMovieById = async (req, res) => {
+    try {
+        const { movieId } = req.params;
+        const movie = await Movie.findById(movieId);
+
+        if (!movie) {
+            return errorResponse(res, 404, 'Not Found', 'Movie not found');
+        }
+
+        return successResponse(res, 200, 'Movie fetched successfully', movie);
+    } catch (error) {
+        console.error('Error fetching movie by ID:', error);
+        return errorResponse(res, 500, 'Internal Server Error', 'An error occurred while fetching the movie');
     }
 }
